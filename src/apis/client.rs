1a65c26592b50f72e2b7c73f6e95e9209c71d3b9
Rodrigo Vaz <rodrigo.vaz@gmail.com>
2020-07-10 18:55:41 -0400

    add path to extensions

--- src/apis/client.rs
+++ src/apis/client.rs
@@ -3,9 +3,9 @@ use std::rc::Rc;
 use super::configuration::Configuration;
 
 pub struct APIClient {
-    catalog_api: Box<crate::apis::CatalogApi>,
-    service_bindings_api: Box<crate::apis::ServiceBindingsApi>,
-    service_instances_api: Box<crate::apis::ServiceInstancesApi>,
+    catalog_api: Box<dyn crate::apis::CatalogApi>,
+    service_bindings_api: Box<dyn crate::apis::ServiceBindingsApi>,
+    service_instances_api: Box<dyn crate::apis::ServiceInstancesApi>,
 }
 
 impl APIClient {
@@ -15,20 +15,21 @@ impl APIClient {
         APIClient {
             catalog_api: Box::new(crate::apis::CatalogApiClient::new(rc.clone())),
             service_bindings_api: Box::new(crate::apis::ServiceBindingsApiClient::new(rc.clone())),
-            service_instances_api: Box::new(crate::apis::ServiceInstancesApiClient::new(rc.clone())),
+            service_instances_api: Box::new(crate::apis::ServiceInstancesApiClient::new(
+                rc.clone(),
+            )),
         }
     }
 
-    pub fn catalog_api(&self) -> &crate::apis::CatalogApi{
+    pub fn catalog_api(&self) -> &crate::apis::CatalogApi {
         self.catalog_api.as_ref()
     }
 
-    pub fn service_bindings_api(&self) -> &crate::apis::ServiceBindingsApi{
+    pub fn service_bindings_api(&self) -> &crate::apis::ServiceBindingsApi {
         self.service_bindings_api.as_ref()
     }
 
-    pub fn service_instances_api(&self) -> &crate::apis::ServiceInstancesApi{
+    pub fn service_instances_api(&self) -> &crate::apis::ServiceInstancesApi {
         self.service_instances_api.as_ref()
     }
-
 }
--- src/models/extension.rs
+++ src/models/extension.rs
@@ -8,6 +8,8 @@
 pub struct Extension {
     #[serde(rename = "id")]
     pub id: String,
+    #[serde(rename = "path")]
+    pub path: String,
     #[serde(rename = "description")]
     pub description: String,
     #[serde(rename = "openapi_url")]
@@ -15,9 +17,10 @@ pub struct Extension {
 }
 
 impl Extension {
-    pub fn new(id: String, description: String, openapi_url: String) -> Extension {
+    pub fn new(id: String, path: String, description: String, openapi_url: String) -> Extension {
         Extension {
             id: id,
+            path: path,
             description: description,
             openapi_url: openapi_url,
         }
